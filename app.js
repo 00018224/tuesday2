1f286c397b6ee00830b540a265a8ee61294e559f
00018224 <abdulazizmukumov@gmail.com>
2025-02-25 16:42:24 +0500

    hshj

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+const port = 3000;
+
+app.get('/', (req,res) => {
+    res.send('<h1>Hello World! (This code supposed to be deployed)</h1>');
+});
+
+app.listen(port, () => {
+    comsole.log('Server is running on port ${port}');
+});
